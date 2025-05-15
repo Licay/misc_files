627659c46622ef947bf48e449aa265062ea9f22a
Licay <40955819+Licay@users.noreply.github.com>
2025-05-15 16:37:33 +0800

    更新 kernel_mk.sh 增加Android工具链环境和固定工作目录
    优化部分执行流程.
    固定工作目录,后每次启动不需重新编译已有部分.
    需添加环境变量$WORK_DIR,启动示例:
    	WORK_DIR=/your_work_dir source kernel_mk.sh kernel_dir config_file

--- scripts/kernel_mk.sh
+++ scripts/kernel_mk.sh
@@ -3,6 +3,7 @@
 # 简单快速创建一个内核编译环境，不污染内核源码，退出时自动移除编译输出目录。
 # 最初的目的是为了给安卓设备快速编译和验证模块代码。
 # eg. source kernel_mk.sh kernel_dir config_file
+#     WORK_DIR=/your_work_dir source kernel_mk.sh kernel_dir config_file
 
 ####### 自动进入工作目录，使用示例（使用m替代make动作）
 # 内核配置
@@ -13,13 +14,19 @@
 # m M=drivers/sample/  modules CONFIG_XXX=m
 
 ########################################################
-###### config that
+###### config that for build kernel
 ARCH=arm64
-CC=aarch64-linux-android26-clang
-CROSS_COMPILE=aarch64-linux-android-
-# CC=clang
-# CROSS_COMPILE=llvm-
+# CC=aarch64-linux-android26-clang
+# CROSS_COMPILE=aarch64-linux-android-
+CC=clang
+M_PARAMS="LLVM=1 LD=ld.lld NM=llvm-nm OBJDUMP=llvm-objdump READELF=llvm-readelf AR=llvm-ar STRIP=llvm-strip OBJSIZE=llvm-size"
+CROSS_COMPILE=
 ########################################################
+
+# export PATH=$PATH:${PREBUILTS_DIR}/linux-x86/clang-r487747c/bin
+# # pahole
+# export PATH=$PATH:${PREBUILTS_DIR}/build-tools/linux-x86/bin
+# export PATH=$PATH:${PREBUILTS_DIR}/aarch64-linux-android-4.9/bin
 
 err_input()
 {
@@ -41,59 +48,65 @@ echo "start $1 check!"
 KER_DIR=$(realpath $1)
 CONFIG_FILE=$(realpath $2)
 
-WORK_DIR=/tmp/kernel.`date +"%Y-%m-%d.%H_%m_%S"`
+if [ "$WORK_DIR" == "" ]; then
+    WORK_DIR=/tmp/kernel.`date +"%Y-%m-%d.%H_%m_%S"`
+    echo "create work dir $WORK_DIR"
+fi
+
 OUT_DIR=$WORK_DIR/out
+KER_SRC=$WORK_DIR/kernel_src
 
 clean_out()
 {
+    echo goodbye!!!
 	rm -rf $WORK_DIR
+    exit 0
 }
 
 # trap "echo ctrl+c!!!; clean_out" SIGINT
-trap "echo goodbye!!!; clean_out" EXIT
+# trap "clean_out" EXIT
 
-mkdir $WORK_DIR
-mkdir $OUT_DIR
-
-# # auto find .config (not support)
-# config_list=$WORK_DIR/config.find
-# find . -type f -name ".config" > $config_list
-# # cat $config_list
-# cnt=1
-# echo "choose your config (default 0)"
-# list=
-# while true ; do
-#     c=`sed -n ${cnt}p $config_list`
-#     if [ "$c" == "" ]; then
-#         break
-#     fi
-#     echo "$cnt: $c"
-#     let "cnt=cnt+1"
-
-# done
-# read var
-# CONFIG_FILE=`pwd`/`sed -n ${var}p $config_list`
-# echo "get $CONFIG_FILE"
-
-# cd $WORK_DIR
+# mkdir $WORK_DIR
+mkdir -p $OUT_DIR
+rm -rf $KER_SRC
+ln -s $KER_DIR $KER_SRC
 
 # 交叉编译
-P0="-C $KER_DIR ARCH=$ARCH O=$OUT_DIR"
+P0="-C $KER_SRC ARCH=$ARCH O=$OUT_DIR"
 P1="CROSS_COMPILE=$CROSS_COMPILE"
 
 P_CC="CC=$CC"
 
-# if needed
-# P_LD="LD=aarch64-linux-android-ld"
-# P_ST="STRIP=aarch64-linux-android-strip"
-# P_OBJ="STRIP=aarch64-linux-android-objdump"
-
-alias sync_config="cp $CONFIG_FILE $OUT_DIR"
-alias m="make $P0 $P1 $P_CC $P_LD $P_ST $P_OBJ"
+alias sync_config="echo sync config file! ;cp $CONFIG_FILE $OUT_DIR/.config"
+alias m="make $P0 $P1 $P_CC $M_PARAMS"
 alias mmc="m menuconfig"
 alias ko="cd $WORK_DIR"
 alias mk="m -j`nproc`"
 
-sync_config
+__help()
+{
+    echo "------------------- kernel build helper -------------------"
+    echo "m: make kernel"
+    echo "mmc: make menuconfig"
+    echo "ko: cd $WORK_DIR"
+    echo "mk: m -j`nproc`"
+    echo "sync_config: sync your config file to $OUT_DIR/.config"
+    echo "clean_out: clean work dir and exit"
+    echo "-----------------------------------------------------------"
+}
 
+if [ -f $OUT_DIR/.config ]; then
+    echo "kernel config file already exist, do you want to sync it? [y/n]"
+    read -r answer
+    if [ "$answer" == "y" ]; then
+        sync_config
+    fi
+else
+    sync_config
+fi
+
+# for user version, make fake abi_symbollist.raw
+touch $OUT_DIR/abi_symbollist.raw
+
+__help
 echo "enjoy it!"
